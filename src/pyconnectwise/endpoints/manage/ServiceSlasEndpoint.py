14637ae2ec576bd42f7a4596f6836debb36068ba
Peter Annabel <peter.annabel@gmail.com>
2025-08-06 22:08:52 -0500

    Added get and pagination to ServicesSLAsEndpoint

--- src/pyconnectwise/endpoints/manage/ServiceSlasEndpoint.py
+++ src/pyconnectwise/endpoints/manage/ServiceSlasEndpoint.py
@@ -3,14 +3,24 @@ from typing import TYPE_CHECKING
 from pyconnectwise.endpoints.base.connectwise_endpoint import ConnectWiseEndpoint
 from pyconnectwise.endpoints.manage.ServiceSlasIdEndpoint import ServiceSlasIdEndpoint
 from pyconnectwise.endpoints.manage.ServiceSlasInfoEndpoint import ServiceSlasInfoEndpoint
+from pyconnectwise.interfaces import IGettable, IPaginateable
+from pyconnectwise.models.manage import SLA
+from pyconnectwise.responses.paginated_response import PaginatedResponse
+from pyconnectwise.types import JSON, ConnectWiseManageRequestParams
 
 if TYPE_CHECKING:
     from pyconnectwise.clients.connectwise_client import ConnectWiseClient
 
 
-class ServiceSlasEndpoint(ConnectWiseEndpoint):
+class ServiceSlasEndpoint(
+    ConnectWiseEndpoint,
+    IGettable[SLA, ConnectWiseManageRequestParams],
+    IPaginateable[SLA, ConnectWiseManageRequestParams],
+):
     def __init__(self, client: "ConnectWiseClient", parent_endpoint: ConnectWiseEndpoint = None) -> None:
         ConnectWiseEndpoint.__init__(self, client, "slas", parent_endpoint=parent_endpoint)
+        IGettable.__init__(self, SLA)
+        IPaginateable.__init__(self, SLA)
 
         self.info = self._register_child_endpoint(ServiceSlasInfoEndpoint(client, parent_endpoint=self))
 
@@ -26,3 +36,35 @@ class ServiceSlasEndpoint(ConnectWiseEndpoint):
         child = ServiceSlasIdEndpoint(self.client, parent_endpoint=self)
         child._id = _id
         return child
+
+    def paginated(
+        self, page: int, page_size: int, params: ConnectWiseManageRequestParams | None = None
+    ) -> PaginatedResponse[SLA]:
+        """
+        Performs a GET request against the /service/slas/{id}/info endpoint and returns an initialized PaginatedResponse object.
+
+        Parameters:
+            page (int): The page number to request.
+            page_size (int): The number of results to return per page.
+            params (dict[str, int | str]): The parameters to send in the request query string.
+        Returns:
+            PaginatedResponse[SLA]: The initialized PaginatedResponse object.
+        """
+        if params:
+            params["page"] = page
+            params["pageSize"] = page_size
+        else:
+            params = {"page": page, "pageSize": page_size}
+        return PaginatedResponse(super()._make_request("GET", params=params), SLA, self, page, page_size, params)
+
+    def get(self, data: JSON | None = None, params: ConnectWiseManageRequestParams | None = None) -> SLA:
+        """
+        Performs a GET request against the /service/slas/{id}/info endpoint.
+
+        Parameters:
+            data (dict[str, Any]): The data to send in the request body.
+            params (dict[str, int | str]): The parameters to send in the request query string.
+        Returns:
+            SLA: The parsed response data.
+        """
+        return self._parse_many(SLA, super()._make_request("GET", data=data, params=params).json())
